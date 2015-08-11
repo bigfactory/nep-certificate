b07bff03868b540b1121fcfce2bb9df80b4dd451
修名 <xiaocong.hxc@alibaba-inc.com>
2015-08-11 16:36:39 +0800

    added ca path

--- bin/gen-cer.cmd
+++ bin/gen-cer.cmd
@@ -2,6 +2,7 @@
 
 set domain=%1
 set outputPath=%2
+set caPrefix=%3
 set commonname=%domain%
  
 set country=ZH
@@ -23,5 +24,5 @@ openssl rsa -in %domain%.key -passin pass:%password% -out %domain%.key
 echo Creating CSR
 openssl req -new -key %domain%.key -out %domain%.csr -passin pass:%password% -subj /C=%country%/ST=%state%/L=%locality%/O=%organization%/OU=%organizationalunit%/CN=%commonname%/emailAddress=%email%
  
-openssl x509 -req -days 3650 -in %domain%.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out %domain%.crt
+openssl x509 -req -days 3650 -in %domain%.csr -CA %caPrefix%.crt -CAkey %caPrefix%.key -CAcreateserial -out %domain%.crt
 echo Finished
